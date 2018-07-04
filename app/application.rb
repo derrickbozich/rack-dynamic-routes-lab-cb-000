378b90edff860d15c0351b9964067414813d03a5
derrick b <dbozich1@gmail.com>
2018-07-04 09:04:47 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    
+
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@item.find {|i| i.name == item_name}
@@ -16,13 +16,13 @@ class Application
       else
         resp.status = 400
         resp.write "Item not found"
-        
+
       end
-    
+
     else
       resp.status = 404
       resp.write "Route not found"
-      
+
     end
     resp.finish
   end
